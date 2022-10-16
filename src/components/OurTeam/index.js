c64b7c9d5001a9b3f7815b9be8d9d5e17b530e78
bhimeshagrawal <bhimeshagrawalggc@gmail.com>
2022-10-16 16:56:11 +0530

    added team page

--- src/components/OurTeam/index.js
+++ src/components/OurTeam/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "../Header";
 import { CoreTeam } from "../../data/CoreTeam";
+import Card from "./Card/Card";
 import { DeveloperTeam } from "../../data/DeveloperTeam";
 import "./styles.css";
 import { Helmet } from "react-helmet";
@@ -49,15 +50,6 @@ class OurTeam extends Component {
               {" "}
               CORE TEAM{" "}
             </a>
-            |{" "}
-            <a
-              className="title"
-              style={{ color: "black" }}
-              onClick={this.scrollToDevRef}
-            >
-              {" "}
-              DEVELOPERS
-            </a>{" "}
           </h2>
         ) : (
           <h2 align="center" style={{ color: "black" }}>
@@ -73,70 +65,9 @@ class OurTeam extends Component {
         )}
         <br />
         <div class="container team">
-          <div class="row  mt-2 mb-2">
+          <div class="row mt-2 mb-2 justify-content-center">
             {CoreTeam.map(person => (
-              <div class="col-sm-6 col-lg-3 my-auto" id={person.name}>
-                <div class="box shadow-sm p-4">
-                  <div class="image-wrapper mb-3">
-                    <img class="img-fluid" src={person.pic} alt={person.name} />
-                  </div>
-                  <div class="box-desc">
-                    <h5>{person.name}</h5>
-                    <p>
-                      {person.post}
-                      <br />
-                      <a href={"tel:+91" + person.phone} className="phone-link">
-                        <i className="fa fa-phone" />
-                        &nbsp;{person.phone}
-                      </a>
-                    </p>
-                  </div>
-                </div>
-              </div>
-            ))}
-          </div>
-          <div class="row mt-4" ref={this.devRef}>
-            {DeveloperTeam.map(person => (
-              <div class="col-sm-6 col-lg-3 my-auto" id={person.name}>
-                <div class="box shadow-sm p-4">
-                  <div class="image-wrapper mb-3">
-                    <img class="img-fluid" src={person.pic} alt={person.name} />
-                  </div>
-                  <div class="box-desc">
-                    <h5>{person.name}</h5>
-                    <p>{person.post}</p>
-                  </div>
-                  <ul class="social">
-                    <li>
-                      <a
-                        href={person.fb}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i class="fa fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={person.git}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i class="fa fa-github"></i>
-                      </a>
-                    </li>
-                    <li>
-                      <a
-                        href={person.linkedin}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        <i class="fa fa-linkedin "></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              <Card member={person}></Card>
             ))}
           </div>
         </div>
